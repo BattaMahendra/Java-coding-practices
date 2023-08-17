3ee8c57a17d33f2296d7f9a8f4168e2962e2a412
BattaMahendra <bmc26g@gmail.com>
2023-08-17 13:21:43 +0530

    Dummy push

--- src/main/java/com/fedex/springdemo/Interview/Executor/Service/SimpleCallable.java
+++ src/main/java/com/fedex/springdemo/Interview/Executor/Service/SimpleCallable.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+
 public class SimpleCallable {
    public static void main(String[] args) {
         ExecutorService executorService=Executors.newFixedThreadPool(10);
