f10fde8384aeb282d6d2d5861b0d184e3338b6b8
saurav.sahoo <saurav101sahoo.sks@yahoo.com>
2023-09-01 17:28:53 +0530

    Added try with resources

--- src/main/java/com/fedex/streamapi/SpliteratorDemo.java
+++ src/main/java/com/fedex/streamapi/SpliteratorDemo.java
@@ -14,14 +14,14 @@ public class SpliteratorDemo {
 
     public static void main(String args[]) throws IOException {
         Path path= Paths.get("C:\\Software Downloads\\Spring\\Java-coding-practices\\src\\main\\java\\com\\fedex\\streamapi\\Employee");
-        Stream<String> lines= Files.lines(path);
-        //lines.forEach(e->System.out.println(e));
-        Stream<String> words=lines.flatMap(line->Arrays.stream(line.split(",")));
-        Spliterator<String> baseSpliterator=words.spliterator();
-        Spliterator<EmployeeObject> spliterator=new EmployeeSpliterator(baseSpliterator);
-        Stream<EmployeeObject> employeeObjectStream=StreamSupport.stream(spliterator,false);
-        List<EmployeeObject> employeeObjectList=employeeObjectStream.collect(Collectors.toList());
-        //employeeObjectStream.forEach(System.out::println);
+        try (Stream<String> lines = Files.lines(path)) {
+			//lines.forEach(e->System.out.println(e));
+			Stream<String> words=lines.flatMap(line->Arrays.stream(line.split(",")));
+			Spliterator<String> baseSpliterator=words.spliterator();
+			Spliterator<EmployeeObject> spliterator=new EmployeeSpliterator(baseSpliterator);
+			Stream<EmployeeObject> employeeObjectStream=StreamSupport.stream(spliterator,false);
+			List<EmployeeObject> employeeObjectList=employeeObjectStream.collect(Collectors.toList());
+			//employeeObjectStream.forEach(System.out::println);
 //        Map<String,List<EmployeeObject>> designationSalaryMap=employeeObjectStream.collect(Collectors.groupingBy(
 //                e->e.getDesignation(),Collectors.toList()
 //        ));
@@ -42,17 +42,18 @@ public class SpliteratorDemo {
 //                    }
 //                })
 //        ));
-        Map<String, Optional<Integer>> maxSalaries =
-                employeeObjectList.stream()
-                        .collect(
-                                Collectors.groupingBy(
-                                        e -> e.getDesignation(),
-                                        Collectors.mapping(
-                                                e -> e.getSalary(),
-                                                Collectors.maxBy(Comparator.comparing(Function.identity()))
-                                        )
-                                )
-                        );
-        System.out.println(maxSalaries);
+			Map<String, Optional<Integer>> maxSalaries =
+			        employeeObjectList.stream()
+			                .collect(
+			                        Collectors.groupingBy(
+			                                e -> e.getDesignation(),
+			                                Collectors.mapping(
+			                                        e -> e.getSalary(),
+			                                        Collectors.maxBy(Comparator.comparing(Function.identity()))
+			                                )
+			                        )
+			                );
+			System.out.println(maxSalaries);
+		}
     }
 }
